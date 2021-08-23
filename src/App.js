2ea3e559bad1a5bc4498422dead42361c398a166
Ashley Follen <ashleyfollen@outlook.com>
2021-08-23 16:24:52 -0600

    created develop branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from 'react';
 
 function App() {
+
+  useEffect(() => {
+    fetch('http://localhost:3000/friends')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello! - testing App v2
     </div>
   );
 }
