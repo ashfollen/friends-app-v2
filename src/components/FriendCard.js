29cc6ef174a578bf6780f9cc792a563881724659
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-08-24 18:28:54 -0700

    Router

--- src/components/FriendCard.js
+++ src/components/FriendCard.js
@@ -1,53 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Chandler from '../audioclips/Chandler.mp3'
+import FriendDetails from './FriendDetails'
 
 
 const FriendCard = ({ friend }) => {
 
-  const playAudio = (name) => {
-    new Audio(`${name}`).play();
+  const [toggle, setToggle] = useState(false);
+
+  function showDetails () {
+    setToggle(!toggle)
   }
+
+  // const playAudio = (name) => {
+  //   new Audio(`${name}`).play();
+  // }
   
   return (
-    <div className="friend-column"  >
-      <div className="friend-card"
-        key={friend.id}
-      >
-        <div className="image">
-          <img alt={friend.name} src={friend.image} />
-        </div>
-        <div className="content">
-          <div className="header">{friend.name}</div>
-          <div className="meta text-wrap">
-            <small>{friend.catchphrase}</small>
-          </div>
-        </div>
-        <div className="extra content">
-          <span>
-            <i className="icon heartbeat" />
-            {friend.about}
-          </span>
-          
-          <span>
-
-            <div className="audio">
-            <audio controls>
-              <source src={Chandler} type="audio/mpeg"/>
-            </audio>
-              <button id={friend.firstName}
-                className="ui mini blue button"
-                // onClick={(e) => playAudio(e.target.id)}
-              >
-               
-              </button>
-            
-
-
-            </div>
-          </span>
-        </div>
-      </div>
+    <div className="Friends-image">  
+        <img alt={friend.name} src={friend.image} 
+        onClick={showDetails}
+        />
+        { toggle ? <FriendDetails friend={friend}  /> : null }
     </div>
+      
   );
 };
 
---
+++ src/components/Home.js
@@ -0,0 +1,6 @@
+
+export default function Home () {
+  return (
+    <img src='https://assets.turbologo.com/blog/en/2019/12/19084751/friends-logo-cover-958x575.png' alt="friends-logo" />
+  );
+}
